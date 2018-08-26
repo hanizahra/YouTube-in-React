49a6629ba1010c17c6d1d3d09822800169952913
Hani Zahra <hanizahra@hanis-mbp-7.home>
2018-08-26 16:51:22 -0400

    restructured index.js, css files

--- youtube-react/src/components/input_search.js
+++ youtube-react/src/components/input_search.js
@@ -5,13 +5,25 @@ class InputSearch extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-
+			term: ''
 		}
+	}
+
+	inputHandler(event) {
+		this.setState({
+			term: event.target.value
+		})
+		console.log('this is term ', this.state.term)
 	}
 
 	render() {
 		return (
-			<input />
+			<div className='search-bar'>
+				<input 
+					onChange={(event) => {this.inputHandler(event)}}
+					value={this.state.term}
+				/>
+			</div>
 		)
 	}
 }
--- youtube-react/src/index.js
+++ youtube-react/src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import InputSearch from './components/input_search';
 import registerServiceWorker from './registerServiceWorker';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <InputSearch />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
