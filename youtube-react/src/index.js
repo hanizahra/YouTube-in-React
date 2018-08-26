8797e4db470d3129bfd8df8a25dfcd59c5eb2b87
Hani Zahra <hanizahra@hanis-mbp-7.home>
2018-08-26 18:29:59 -0400

    added axios

--- youtube-react/src/index.js
+++ youtube-react/src/index.js
@@ -1,24 +1,32 @@
 import dotenv from 'dotenv';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import InputSearch from './components/input_search';
 import VideoDetail from './components/video_detail';
 import VideoList from './components/video_list';
 dotenv.config()
-console.log('env: ', +process.env)
+const API_KEY = process.env.REACT_APP_API_KEY
 
 class App extends Component {
   constructor(props) {
   	super(props);
   	this.state = {
-  		apikey: process.env.REACT_APP_API_KEY
   	}
+  	this.youTubeSearch();
+  }
+
+  youTubeSearch() {
+  	axios.get('https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyDhW0fadhsCTrSc1jaHKa0tJ6J6paXyvDs&q=dogs')
+  		.then(response => {
+  			console.log('this is the response ', response.data)
+  		})
   }
 
   render() {
-  	console.log('this is api key ', this.state.apikey)
+
     return (
       <div>
         <InputSearch />
