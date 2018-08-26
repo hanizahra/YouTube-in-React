1c9390b9cf36182bc703d39ab25cc115c02ddf2f
Hani Zahra <hanizahra@hanis-mbp-7.home>
2018-08-26 16:33:34 -0400

    Created components and started input_search

---
+++ youtube-react/src/components/input_search.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class InputSearch extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return (
+			<input />
+		)
+	}
+}
+
+export default InputSearch;
