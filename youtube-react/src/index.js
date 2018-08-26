344601b88e188ab1aa446afeb2938861fc2caa47
Hani Zahra <hanizahra@hanis-mbp-7.home>
2018-08-26 17:47:36 -0400

    set up api key and dotenv

--- youtube-react/src/index.js
+++ youtube-react/src/index.js
@@ -1,17 +1,29 @@
+import dotenv from 'dotenv';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import InputSearch from './components/input_search';
 import VideoDetail from './components/video_detail';
-
+import VideoList from './components/video_list';
+dotenv.config()
+console.log('env: ', +process.env)
 
 class App extends Component {
+  constructor(props) {
+  	super(props);
+  	this.state = {
+  		apikey: process.env.REACT_APP_API_KEY
+  	}
+  }
+
   render() {
+  	console.log('this is api key ', this.state.apikey)
     return (
       <div>
         <InputSearch />
         <VideoDetail />
+        <VideoList />
       </div>
     );
   }
