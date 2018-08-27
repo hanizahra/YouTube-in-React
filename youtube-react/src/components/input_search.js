e79ebee5f39bfc25b98f2fa5cdc55754bcda7bba
Hani Zahra <hanizahra@hanis-mbp-7.home>
2018-08-27 00:03:54 -0400

    refactored input_search.js

--- youtube-react/src/components/input_search.js
+++ youtube-react/src/components/input_search.js
@@ -1,31 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 
-class InputSearch extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			term: ''
-		}
+const InputSearch = (props) => {
+	
+	const inputHandler = (event) => {
+		props.searched(event.target.value)
 	}
 
-	inputHandler(event) {
-		this.setState({
-			term: event.target.value
-		})
-		this.props.searched(event.target.value)
-	}
-
-	render() {
-		return (
-			<div className='search-bar'>
-				<input 
-					value={this.state.term}
-					onChange={(event) => {this.inputHandler(event)}}
-				/>
-			</div>
-		)
-	}
+	return (
+		<div className='search-bar'>
+			<input 
+				onChange={(event) => {inputHandler(event)}}
+			/>
+		</div>
+	)
 }
 
 export default InputSearch;
